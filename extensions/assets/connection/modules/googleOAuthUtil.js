f95c2b326ab301633fafd25b9ad2ace5e8593253
ravindraranwala <ravindraranwala@gmail.com>
2014-10-01 19:41:53 +0530

    registry utility was support and google connection was added.

---
+++ extensions/assets/connection/modules/googleOAuthUtil.js
@@ -0,0 +1,41 @@
+/**
+ * Created by ravindra on 10/1/14.
+ */
+var api = {};
+(function () {
+    var provider = {
+        "oauth_version": "2",
+        "authorization_url": "https://accounts.google.com/o/oauth2/auth",
+        "access_token_url": "https://accounts.google.com/o/oauth2/token",
+        "api_key": "212972391932-3thmh0maoodoin7t116vb0adpjjvckv0.apps.googleusercontent.com",
+        "api_secret": "PY9hUuKKwxj_D92LbawosUKl",
+        "callback_url": "https://localhost:9443/recipe-app/asts/connection/googleSuccess?name=googlespreadsheet&type=oauth&version=2",
+        "authorize_params": {
+            "state": "wso2recipetrial",
+            "scope": "https://www.googleapis.com/auth/drive.readonly https://spreadsheets.google.com/feeds/",
+            "access_type": "offline",
+            "approval_prompt": "force"
+        }            };
+
+    api.getGoogleProviderConfig = function () {
+        return provider;
+    }
+
+    api.getRedirectUrl = function(){
+        return provider.callback_url;
+    }
+
+    api.getClientId = function(){
+        return provider.api_key;
+    }
+
+    api.getClientSecret = function(){
+        return provider.api_secret;
+    }
+    
+    api.getAccessTokenUrl = function () {
+        return provider.access_token_url;
+    }
+
+}());
+
---
+++ extensions/assets/connection/modules/registryUtil.js
@@ -0,0 +1,50 @@
+/**
+ * Created by ravindra on 10/1/14.
+ * This is used to manipulate carbon registry. All the read, update and delete operation against the registry should use these utility methods defined here.
+ */
+var api = {};
+(function () {
+    var carbon = require('carbon');
+    var url = 'https://localhost:9443/admin/carbon/';
+    var server = new carbon.server.Server(url);
+    var options = {
+        username: 'admin',
+        domain: 'carbon.super',
+        tenantId: -1234
+    };
+    var registry = new carbon.registry.Registry(server, options);
+    var dataStore = new MetadataStore("admin", "admin");
+
+    var getAccountsContent = function (path) {
+        // log.info(path);
+        var res;
+        // We assume that at least an empty Account file exists in the registry at this point.
+        res = dataStore.get(path);
+        return res.content;
+    };
+
+    api.insertNewAccount = function (path, newAcct, newAuthInfo) {
+        var log = new Log();
+        var accountContent = getAccountsContent(path);
+
+        var acctContentJSON = parse(accountContent);
+
+        // If there's no any user account with the given name then create one.
+        if (!acctContentJSON.hasOwnProperty(newAcct)) {
+            // Setting the JSON contents here.
+            acctContentJSON[newAcct] = parse(newAuthInfo);
+
+            var res = dataStore.get(path);
+            // Setting the registry resource here.
+            res.content = stringify(acctContentJSON);
+
+            // updating the registry with the new resource here.
+            dataStore.put(path, res);
+        }
+    };
+
+    api.deleteAccount = function (path, acct) {
+        // TODO fill this method.
+        var accountContent = getAccountsContent(path);
+    };
+}());
