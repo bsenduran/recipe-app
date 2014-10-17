a7cde8c802e1a4cdbaacd36f0436b21626d071f3
ravindraranwala <ravindraranwala@gmail.com>
2014-10-17 10:24:31 +0530

    Google gmail OAuth 2 support was refactpred to use only one success page along with other google APIs such as spreadsheet API.

--- extensions/assets/connection/modules/googleMailOAuthUtil.js
+++ extensions/assets/connection/modules/googleMailOAuthUtil.js
@@ -6,7 +6,7 @@ var api = {};
         "access_token_url": "https://accounts.google.com/o/oauth2/token",
         "api_key": "212972391932-3thmh0maoodoin7t116vb0adpjjvckv0.apps.googleusercontent.com",
         "api_secret": "PY9hUuKKwxj_D92LbawosUKl",
-        "callback_url": "https://localhost:9443/recipe-app/asts/connection/googleMailSuccess?type=oauth&version=2",
+        "callback_url": "https://localhost:9443/recipe-app/asts/connection/googleSuccess?name=googlespreadsheet&type=oauth&version=2",
         "authorize_params": {
             "state": "wso2recipetrial",
             "scope": "https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/userinfo.email",
